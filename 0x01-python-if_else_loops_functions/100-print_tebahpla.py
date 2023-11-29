1b948146d3e232fefa62b12c76880a12f0c52e2c
tasneem04 <eng.wr.tasneem@gmail.com>
2023-11-29 07:58:43 -0800

    Fix 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(ord('z'), ord('a')-1, -1):
-    print("{}".format(chr(i)), end='')
+    if i % 2 != 0:
+        print("{}".format(chr(i-32)), end='')
+    else:
+        print("{}".format(chr(i)), end='')
