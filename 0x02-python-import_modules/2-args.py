51cabd3c10eab4917ed4f45d27d3adfadad3b2f8
tasneem04 <eng.wr.tasneem@gmail.com>
2023-11-30 13:46:26 -0800

    Fix 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     argv = len(sys.argv) - 1
     if argv == 0:
         print("{} arguments.".format(argv))
+    elif argv == 1:
+        print("{} argument:".format(argv), end='\n')
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} argument:".format(argv), end='\n')
         for i in range(1, (argv + 1)):
