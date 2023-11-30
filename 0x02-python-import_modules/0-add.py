81fb4cd8919b4ed6c7f2f6595a23a957901e76bc
tasneem04 <eng.wr.tasneem@gmail.com>
2023-11-30 03:34:53 -0800

    Fix code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 from add_0 import a, b, add
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)), end='')
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -3,6 +3,7 @@
 a = 1
 b = 2
 
+
 def add(a, b):
     """My addition function
 
