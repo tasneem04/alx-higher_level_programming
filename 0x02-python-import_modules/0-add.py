3bb54bde03e3fc42757ad5a4326c279a62cc8580
tasneem04 <eng.wr.tasneem@gmail.com>
2023-11-30 05:51:00 -0800

    Fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import a, b, add
-    print("{} + {} = {}".format(a, b, add(a, b)), end='')
+    print("{} + {} = {}".format(a, b, add(a, b)), end='\n')
