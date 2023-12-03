c8e91b25f594a5640ac3246c42bd5a3ec9860941
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-03 07:19:24 -0800

    Fix 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,7 @@ def new_in_list(my_list, idx, element):
     for i in range(0, len(cp_list)):
         if idx < 0:
             return cp_list
-        elif idx > len(cp_list):
+        elif idx >= len(cp_list):
             return cp_list
         else:
             cp_list[idx] = element
