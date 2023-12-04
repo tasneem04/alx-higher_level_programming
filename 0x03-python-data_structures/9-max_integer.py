9fda7af9a6a79d3b4c19002b55d50dfdc30ab97c
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-04 02:49:06 -0800

    Fix 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,11 @@
 def max_integer(my_list=[]):
     if len(my_list) != 0:
         for i in range(0, (len(my_list)-1)):
-            if my_list[i] >=  my_list[i+1]:
-                return my_list[i]
+            for j in range(0 ,(len(my_list)-1)):
+                if my_list[i] >=  my_list[j+1]:
+                    return my_list[i]
+                else:
+                    return my_list[j+1]
     else:
         return None
         
