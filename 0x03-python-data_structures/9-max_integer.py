54daaf99167740b437ce1f92cf82db9a88f149e4
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-04 03:08:55 -0800

    Fix 9 used sort insted of for loop

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) != 0:
-        for i in range(0, (len(my_list)-1)):
-            for j in range(0 ,(len(my_list)-1)):
-                if my_list[i] >=  my_list[j+1]:
-                    return my_list[i]
-                else:
-                    return my_list[j+1]
+        cp_list = my_list.copy()
+        cp_list.sort()
+        return cp_list[-1]
     else:
         return None
         
