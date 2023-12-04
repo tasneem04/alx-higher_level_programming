deb75ea031090fdff0aa304ffa71f9ee3f7e95d7
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-04 14:17:06 -0800

    Fix 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
         return None
     cp_list = []
     for i in my_list:
-        if (my_list[i] % 2) == 0:
+        if (i % 2) == 0:
             cp_list.append(True)
         else:
             cp_list.append(False)
