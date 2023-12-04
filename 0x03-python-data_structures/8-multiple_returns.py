889333b41e37202d3b5ad6dc98b46c8426c7ec54
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-04 02:19:41 -0800

    Fix 8 by removing for

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for item in sentence:
-        if len(sentence) != 0:
-            result = tuple((len(sentence), sentence[0]))
-            return result
-        else:
-            result = tuple((len(sentence), None))
-            return result
-
+    if len(sentence) != 0:
+        result = tuple((len(sentence), sentence[0]))
+        return result
+    else:
+        result = tuple((len(sentence), None))
+        return result
