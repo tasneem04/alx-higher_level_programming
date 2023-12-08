c0a2f9e0e2808127f07c011e08f10f3ddf7982f0
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-08 15:59:57 -0800

    Fix 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    for key in a_dictionary:
+        del a_dictionary[key]
+        return a_dictionary
     return a_dictionary
 
