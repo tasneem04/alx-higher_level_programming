a74d3dfa41532821ccd9143d3377065811ab0ea7
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-11 01:08:46 -0800

    Fix z

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-def uppercase(str) :
-    for i in range(0 ,len(str)):
-         print("{}".format(chr(ord(str[i])-32)) if ord(str[i]) in range(ord('a'), ord('z')) else chr (ord(str[i])), end='')
-
+def uppercase(str):
+    for i in range(0, len(str)):
+         print("{}".format(
+            chr(ord(str[i])-32)) if ord(str[i]) in range(97, 123) else chr(
+            ord(str[i])), end='')
     print(end='\n')
