a77e14abb604b510e9eed6cdfaabc9ef3418c3dd
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-18 11:34:53 -0800

    Delete newLine

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     if isinstance(value, int):
         try:
-            print("{:d}".format(value), end='\n')
+            print("{:d}".format(value))
         except(ValueError, TypeError):
             return False
         else:
