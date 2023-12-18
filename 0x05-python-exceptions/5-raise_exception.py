c23832a16c73ba856fb42fd1fd1e8242451e95c9
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-18 12:46:07 -0800

    Fix the error of replacing 5-main

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    raise_exception = __import__('5-raise_exception').raise_exception
-    try:
-        raise_exception()
-    except TypeError as te:
-        print("Exception raised")
+    raise TypeError
