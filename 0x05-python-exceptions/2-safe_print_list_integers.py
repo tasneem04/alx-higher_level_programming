91d2dc585a959badf17f673416f831747105680c
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-18 12:05:33 -0800

    Fix codeError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-     count = 0
+    count = 0
     for i in range(0, x):
         try:
             if isinstance(my_list[i], int):
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
         except (TypeError, ValueError):
             pass
-    print()
+        print()
     return count
