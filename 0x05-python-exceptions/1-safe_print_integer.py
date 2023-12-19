708c3a380407e64ddae91567153004017a4a6835
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-18 16:05:05 -0800

    Remove isIncstase

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if isinstance(value, int):
-        try:
-            print("{:d}".format(value))
-        except (ValueError, TypeError):
-            return False
-        else:
-            return True
+    try:
+        print("{:d}".format(value))
+    except:
+        return False
+    return True
