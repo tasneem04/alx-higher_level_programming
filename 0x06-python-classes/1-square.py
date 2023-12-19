3e51a2697724e037af7f20178339b88d4b7d32f0
tasneem04 <eng.wr.tasneem@gmail.com>
2023-12-19 07:39:42 -0800

    Add 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """ This is a Square Class """
+    def __init__(self, size):
+        """ defining the size of square """
+        self.__size = size
