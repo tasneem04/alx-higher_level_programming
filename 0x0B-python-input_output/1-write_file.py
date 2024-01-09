2aa2e8be79a692fdf840d3744e982160e48cc2a3
tasneem04 <eng.wr.tasneem@gmail.com>
2024-01-09 06:15:31 -0800

    Add 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,10 +2,17 @@
 """Module containing the function write_file"""
 
 
-def number_of_lines(filename=""):
-    """returns number of lines in a file"""
-    lines = 0
-    with open(filename, mode='r', encoding='utf-8') as a_file:
-        for i, l in enumerate(a_file):
-            lines += 1
-    return lines
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number
+    of characters written.
+
+    Args:
+        filename (str, optional): name of the file. Defaults to "".
+        text (str, optional): string of text to write to file. Defaults to "".
+
+    Returns:
+        int: number of characters written to file.
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        """This method returns the number of characters written to a file."""
+        return f.write(text)
