eebb82f9e88316733034940d5089e70a02782c40
tasneem04 <eng.wr.tasneem@gmail.com>
2024-01-09 06:10:37 -0800

    Fix 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module containing the function read_file"""
+
+
 def read_file(filename=""):
     """reads text file"""
     with open(filename, mode='r', encoding='utf-8') as a_file:
