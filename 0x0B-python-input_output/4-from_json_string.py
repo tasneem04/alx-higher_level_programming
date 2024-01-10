005fe4e6aefd4067ee1e65d716fff7fec0b7f344
tasneem04 <eng.wr.tasneem@gmail.com>
2024-01-10 10:15:14 -0800

    Add 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module containing the function from_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string.
+    Returns:
+        type: Python object.
+    """
+    # print("type json.loads(my_str)--> {}".format(type(json.loads(my_str))))
+    # print("type my_str--> {}".format(type(my_str)))
+    return json.loads(my_str)
